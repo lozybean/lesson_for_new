a29dab0252e1209031c0a3624ed209f708fba0a8
Lyon <lozybean@163.com>
2015-07-06 13:39:22 +0800

    init

---
+++ perl_scalar.pl
@@ -0,0 +1,15 @@
+#标量中数字和字符串的自动转换；
+print "标量的上下文：\n";
+$a = '5GeRen';
+$b = '10GePingguo';
+print $a+$b;
+print "\n";
+print "$a+$b\n";
+$a = 10;
+$b = $a;
+print \$a;
+print \$b;
+
+print "常用内置标量：\n";
+
+print '$/:'."$/\n";
